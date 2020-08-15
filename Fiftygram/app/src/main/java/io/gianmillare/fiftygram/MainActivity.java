85b291aeda7ba51a2860b13a1aba3cba686339d2
gianmillare <gianmillaregsm@gmail.com>
2020-08-15 16:26:10 -0700

    created a onActivityResult method to pull an image in the form of data

--- Fiftygram/app/src/main/java/io/gianmillare/fiftygram/MainActivity.java
+++ Fiftygram/app/src/main/java/io/gianmillare/fiftygram/MainActivity.java
@@ -1,15 +1,27 @@
 package io.gianmillare.fiftygram;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+
+import java.io.FileDescriptor;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 //    Reference the image view created in activity_main.xml
     private ImageView imageView;
+    private Bitmap image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,5 +40,26 @@ public class MainActivity extends AppCompatActivity {
         intent.setType("image/*");
 //        start the activity
         startActivityForResult(intent, 1);
+    }
+
+//    Create a method that returns a new activity after the above method
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+//        this method should only execute with the required requestCode and the data is not null
+        if (resultCode == Activity.RESULT_OK && data != null) {
+            try {
+//            pass a URI that contains the path to the data you are pulling (an image on the desk)
+                Uri uri = data.getData();
+//            pull the data from the path
+                ParcelFileDescriptor parcelFileDescriptor = getContentResolver().openFileDescriptor(uri, "r");
+                FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+                image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+                parcelFileDescriptor.close();
+                imageView.setImageBitmap(image);
+            } catch (IOException e) {
+                Log.e("cs50", "Image not found", e);
+            }
+        }
     }
 }
